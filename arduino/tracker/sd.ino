912f3ef145aa500bddcf903acc5821e2a28ae03a
Christian <max246@gmail.com>
2015-04-25 22:08:50 +0100

    add working GPS and SDCARD Writing

---
+++ arduino/tracker/sd.ino
@@ -0,0 +1,35 @@
+void createNewFile() {
+    File fileNumber = SD.open("last",  FILE_READ);
+    String number = "";
+    if (fileNumber) {
+        while (fileNumber.available()) {
+            number.concat((char)fileNumber.read());
+        }
+        fileNumber.close();
+        int num = number.toInt()+1;
+        writeLastNumber(num);
+        #ifdef DEBUG
+        Serial.print("Numberation file increased: ");
+        #endif
+        FILENAME = "data";
+        FILENAME.concat(num);
+        FILENAME.concat(".txt");
+        #ifdef DEBUG
+        Serial.println(FILENAME);
+        #endif
+    }  else {
+      #ifdef DEBUG
+      Serial.println("No numberation file");
+      #endif
+      FILENAME = "data1.txt";
+      writeLastNumber(1);
+    }
+}
+
+void writeLastNumber(int number) {
+    File fileNumber = SD.open("last",  O_WRITE | O_CREAT | O_TRUNC); //Trunc will set the length to 0, equal to erase the file
+    if (fileNumber) {
+       fileNumber.print(number);
+    }
+    fileNumber.close();
+}
--- arduino/tracker/tracker.ino
+++ arduino/tracker/tracker.ino
@@ -1,49 +1,92 @@
-#include <TinyGPS.h>
-#include <SD.h>
+#include <TinyGPS++.h>
 
+#define SDWRITE
+#define DEBUG
 
-TinyGPS gps;
-
+#ifdef SDWRITE
+  #include <SD.h> 
+#endif
+//SDCARD SETUP
 const int chipSelect = 4;
+String FILENAME = "data1.txt"; //Default filename
+
+TinyGPSPlus gps;
+boolean gps_ready = false;
+
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial1.begin(9600);
   
+  
+  #ifdef SDWRITE
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
+  pinMode(chipSelect, OUTPUT);
   
-    if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
     return;
   }
   Serial.println("card initialized.");
+  createNewFile();
+  
+  #endif
 }
   
   
 
 
 void loop() {
-  float flat, flon;
-  unsigned long age = 0;
   
-  print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
-  gps.f_get_position(&flat, &flon, &age);
-  print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
-  print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  Serial.println();
-  
-   File dataFile = SD.open("datalog.txt", FILE_WRITE);
-   
-    if (dataFile) {
-    dataFile.println(flat);
-    dataFile.close();
+#ifdef DEBUG 
+   if (gps_ready) {
+     Serial.print(F("Location: ")); 
+     if (gps.location.isValid()) {
+         Serial.print(gps.location.lat(), 6);
+         Serial.print(F(","));
+         Serial.print(gps.location.lng(), 6);
+     } else 
+         Serial.print(F("INVALID"));
+     Serial.println();
+   }
+#endif
+#ifdef SDWRITE   
+ char tmpFile[FILENAME.length()+1];
+ FILENAME.toCharArray(tmpFile, sizeof(tmpFile));
+ File dataFile = SD.open(tmpFile,  O_CREAT | O_APPEND | O_WRITE); //Append the file 
+ //Make sure that file is ready and gps as well
+ if (dataFile) {
+    if (gps_ready) {
+    #ifdef DEBUG
+    Serial.println("Location wrote into the file");
+    #endif
+    if (gps.location.isValid()) {
+      dataFile.print("la:");
+      dataFile.print(gps.location.lat(),6);
+      dataFile.print("lg:");
+      dataFile.print(gps.location.lng(),6);
+      
+      dataFile.print("d:");
+      if (gps.date.isValid()) 
+          dataFile.print(gps.date.value());
+      else 
+         dataFile.print("-1"); 
+          
+      dataFile.print("t:");
+      if (gps.time.isValid())
+         dataFile.println(gps.time.value());
+      else
+         dataFile.println("-1");
     }
+    }
+    dataFile.close();
+ }
+#endif
   
-  smartdelay(1000);
+ readGPS();
 }
 
